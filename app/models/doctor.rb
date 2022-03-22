571119d71d1c2a5fab8b86a780efa0410b44c590
Yevhenii Kononenko <evgenijkononenko0@gmail.com>
2022-03-22 13:19:17 +0200

    Initial gems setup

---
+++ app/models/doctor.rb
@@ -0,0 +1,5 @@
+class Doctor < ApplicationRecord
+  has_many :appointments
+  has_many :users, through: :appointments
+  belongs_to :category
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+ has_many :appointments
+ has_many :doctors, through: :appointments
+
+ validates :phone, uniqueness: true
+
+ def email_required?
+  false
+ end
+ 
+ def email_changed?
+  false
+ end
+
+
 end
